dbfa97d4eecc3cb96e2d142c69061dcc20fd19ba
Erik0631 <111729359+ErikAJensen@users.noreply.github.com>
2024-04-30 11:46:06 +0200

    legger api i MovieCard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,14 @@
 import { useState, useEffect } from "react"
-import "./App.css"
+import React from "react";
+import MovieCard from "./components/MovieCard";
+import "./App.css";
 
-export default function App() {
-  const [content, setContent] = useState("")
-  // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
-
-  const url =
-    "https://moviesdatabase.p.rapidapi.com/titles/search/title/the%20shining?exact=false&titleType=movie"
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  }
-
-  const getMovie = async () => {
-    try {
-      const response = await fetch(url, options)
-      const result = await response.json()
-      console.log(result)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    getMovie()
-  }, [])
-
-  return <></>
+function App() {
+  return (
+    <div className="App">
+      <MovieCard />
+    </div>
+  );
 }
+
+export default App;
---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+function MovieCard() {
+  const [content, setContent] = useState("");
+  // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
+
+  const url =
+    "https://moviesdatabase.p.rapidapi.com/titles/search/title/the%20shining?exact=false&titleType=movie";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    },
+  };
+
+  const getMovie = async () => {
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return <></>;
+}
+
+export default MovieCard;
