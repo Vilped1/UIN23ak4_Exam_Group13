37d30277d0cf5cd8a591e5f22234b0b7b787dd4d
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-04-30 10:27:24 +0200

    tester noe api

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
 function App() {
-  const [count, setCount] = useState(0)
+  const apiKey = process.env.REACT_APP_API_KEY;
 
-  return (
-    <>
-     
-    </>
-  )
+  const [count, setCount] = useState(0);
+
+  return <></>;
 }
 
-export default App
+export default App;
