f8556e7803b96c995c4afaa9fb5dd60b8f6f6f20
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-04-30 11:09:24 +0200

    leker med APIet fordi det er gøy!!

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,65 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
-function App() {
-  const apiKey = process.env.REACT_APP_API_KEY;
+const url =
+  "https://moviesdatabase.p.rapidapi.com/titles/search/title/the%20shining?exact=true&titleType=movie";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
+    "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  },
+};
 
-  const [count, setCount] = useState(0);
+try {
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
+
+function App() {
+  const [content, setContent] = useState("");
+
+  /*   const fetchData = async () => {
+    try {
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const response = await fetch(
+        `https://moviesdatabase.p.rapidapi.com/titles?genre=Action`
+      );
+      const data = await response.json();
+      setContent(data.results);
+
+      console.log(content);
+    } catch {
+      console.error("Det har skjedd en feil");
+    }
+  }; */
+
+  /*   const url = "https://moviesdatabase.p.rapidapi.com/titles?genre=Action";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+    },
+  };
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setContent(data);
+      console.log(content);
+    } catch (error) {
+      console.error(error);
+    } */
+
+  /*   useEffect(() => {
+    fetchData();
+  }, []); */
 
   return <></>;
 }
