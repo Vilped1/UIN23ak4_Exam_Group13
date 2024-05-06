351508f7d754d808d232b32d5729c22fa83156fc
Vilped1 <vildeandrea01@gmail.com>
2024-05-06 11:14:41 +0200

    Update movieServices.js

--- Frontend/sanity/services/movieServices.js
+++ Frontend/sanity/services/movieServices.js
@@ -3,7 +3,10 @@ import { client } from '../client'
 export async function fetchMovies() {
     const data = await client.fetch(`*[_type == "movies"]{
         _id,
-        title,
+        id,
+        primaryImage,
+        titleText,
+        releaseYear,
         "movieurl": movieurl.current
     }`)
 }
