5ed7e6be37885ebc2a1e319d4ef0f08efdf7853a
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-05-06 14:02:03 +0200

    Endelig fetch av sjanger fra sanity
    å herre milde jesus

--- Frontend/sanity/services/genreServices.js
+++ Frontend/sanity/services/genreServices.js
@@ -1,8 +1,11 @@
 import { client } from "../client";
 
+/* https://www.sanity.io/docs/how-queries-work#dd66cae5ed8f */
 export async function fetchAllGenres() {
-  const genreData = await client.fetch(`*[_type == "genre"]{
-    genre,
+  const genreData = await client.fetch(`*[_type == "asset"] | order(imagetitle asc){
+    _id,
+    "image": image.asset->url,
+    imagetitle,
     }`)
   return genreData;
 }
--- Frontend/src/components/GenreSection.jsx
+++ Frontend/src/components/GenreSection.jsx
@@ -18,64 +18,24 @@ export default function GenreSection() {
     console.log(genre);
   }, []);
 
-  /*   const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  };
-
-  const getGenres = async () => {
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      console.log(data.results);
-      setGenre(data.results.slice(1));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    getGenres();
-  }, []); */
-
   return (
     <>
-      {/*      <section className="genreSection">
-        {genre.map((genreName, index) => {
-          const genreInfo = resources.find(
-            (genreItem) => genreItem.name === genreName
-          );
-          return (
-            <article
-              className="genreCard"
-              key={index}
-              style={{
-                backgroundImage: `url(${genreInfo ? genreInfo.imageUrl : ""})`,
-              }}
-            >
-              <h3>{genreName}</h3>
-            </article>
-          );
-        })}
-      </section>*/}
-
+      {/*   TIPS TIL UTFORMING
+      https://www.sanity.io/docs/image-urls
+      https://www.sanity.io/docs/presenting-images
+      https://www.sanity.io/docs/how-queries-work#dd66cae5ed8f */}
       <section className="genreSection">
-        {genre.map((genre, i) => {
+        {genre.map((genre, index) => (
           <article
             className="genreCard"
-            key={i}
+            key={index}
             style={{
-              /*https://www.sanity.io/docs/image-urls*/
-              backgroundImage: `url(https://cdn.sanity.io/asset/s2614gkl/production/${genre.image.asset._ref}h=75)`,
+              backgroundImage: `url(${genre.image}?h=300)`,
             }}
           >
             <h3>{genre.imagetitle}</h3>
-          </article>;
-        })}
+          </article>
+        ))}
       </section>
     </>
   );
