8570ae7f5ef88716889282a78b776824b73f70aa
thoraho <thoraho@hiof.no>
2024-05-07 09:24:04 +0200

    updated genre fetch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,37 +5,29 @@ import { FetchUser } from "../sanity/services/userServices"
 
 export default function App() {
   const [content, setContent] = useState(null)
+  const [genres, setGenres] = useState(null)
   // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
-
-  const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  }
 
   const getGenres = async () => {
     try {
-      const response = await fetch(url, options)
-      const data = await response.json()
-      setContent(data.results)
-      console.log(data.results)
-    } catch (error) {
-      console.error(error)
+      const data = await fetchGenres()
+      setGenres(data)
+    } catch {
+      console.log("neeiii")
     }
   }
 
   useEffect(() => {
     getGenres()
-    fetchGenres().then((data) => {
-      console.log(data)
-    })
   }, [])
 
-
   return (
-   <></>
+    <>
+      {genres?.map((item, index) => (
+        <div key={index}>
+          <h2>{item.genre}</h2>
+        </div>
+      ))}
+    </>
   )
 }
