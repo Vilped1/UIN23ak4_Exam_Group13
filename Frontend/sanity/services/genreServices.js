7ee2949fbb0097f9ee33f1919d4af7db4525cc2f
Erik0631 <erik12jensen@hotmail.com>
2024-05-18 12:42:58 +0100

    rydder i koden for bedre struktur

--- Frontend/sanity/services/genreServices.js
+++ Frontend/sanity/services/genreServices.js
@@ -1,11 +1,32 @@
-import { client } from '../client'
+import { client } from '../client';
 
+// Henter alle sjangere
 export async function fetchGenres() {
     const data = await client.fetch(`*[_type == "genres"]{
         _id,
         genre,
         "genreurl": genreurl.current
-    }`)
-    console.log(data)
-    return data
+    }`);
+    console.log(data);
+    return data;
+}
+
+// Henter favorittfilmer og favorittsjangere for en spesifikk bruker fra Sanity
+export async function FetchUserFavorites(user) {
+    try {
+        const data = await client.fetch(`*[_type == "users" && user == $user] {
+            "favoriteGenre": favoriteGenre[]->{
+                _id,
+                genre
+            }, // Favorittsjangre
+            "favoriteMovies": favoriteMovies[]->{
+                movietitle,
+                "movieurl": movieurl.current
+            } // Favorittfilmer med tittel og URL
+        }`, { user });
+        return data;
+    } catch (error) {
+        console.error('Error fetching user favorites:', error);
+        return null;
+    }
 }
--- frontend/sanity/services/userServices.js
+++ frontend/sanity/services/userServices.js
@@ -1,28 +1,15 @@
 import { client } from '../client';
 
-//Henter alle brukere fra Sanity
+// Henter alle brukere fra Sanity
 export async function FetchUser() {
   try {
     const data = await client.fetch(`*[_type == "users"] {
       _id, // Bruker-ID
       user, // Brukernavn
-      "favoriteGenre": favoriteGenre[]->genre, // Favorittsjangre
     }`);
     return data; 
   } catch (error) {
+    console.error('Error fetching users:', error);
     return null;
-  }
-}
-
-//Henter favorittfilmer for en spesifikk bruker fra Sanity
-export async function FetchUserFavorites(user) {
-  try {
-    const data = await client.fetch(`*[_type == "users" && user == $user] {
-      "favoriteGenre": favoriteGenre[]->genre, // Favorittsjangre
-      "favoriteMovies": favoriteMovies[]->{movietitle, movieurl} // Favorittfilmer med tittel og URL
-    }`, { user }); 
-    return data; 
-  } catch (error) {
-    return null; 
   }
 }
