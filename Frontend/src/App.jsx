06f9f127f9c5805b33f5af4c57ba69c089852329
Erik0631 <111729359+ErikAJensen@users.noreply.github.com>
2024-05-18 08:24:09 +0200

    prøver å forkorte koden litt

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,36 +1,33 @@
-  import { useState, useEffect } from "react";
-  import React from "react";
-  import { Route, Routes, useNavigate } from "react-router-dom";
-  import Layout from "./components/Layout";
-  import Home from "./components/Home";
-  import Genres from "./components/Genres";
-  import UserCompare from "./components/UserCompare";
-  import OneGenre from "./components/OneGenre";
-  import FrontPage from "./components/FrontPage";
-  import { FetchUser, FetchUserFavorites } from "../sanity/services/userServices";
+import { useState, useEffect } from "react";
+import React from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import Genres from "./components/Genres";
+import UserCompare from "./components/UserCompare";
+import OneGenre from "./components/OneGenre";
+import FrontPage from "./components/FrontPage";
+import { FetchUser, FetchUserFavorites } from "../sanity/services/userServices";
 
 export default function App() {
-
-  const [content, setContent] = useState(null); 
-  const [users, setUsers] = useState([]); 
-  const [compareUser, setCompareUser] = useState(null); 
+  const [content, setContent] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [compareUser, setCompareUser] = useState(null);
   const [activeUser, setActiveUser] = useState(null);
-  const [favoriteMovies, setFavoriteMovies] = useState([]); 
-  const navigate = useNavigate(); // Navigasjonsfunksjon
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const navigate = useNavigate();
 
-  // Hent brukerliste
   useEffect(() => {
     FetchUser().then((data) => {
       setUsers(data);
     });
   }, []);
 
-  // Klikk på en bruker for å hente og vise favorittfilmer
   const handleUserClick = async (user) => {
-    setActiveUser(user.user); // aktiv bruker
-    const userFavorites = await FetchUserFavorites(user.user); // Henter favorittfilmer
-    setFavoriteMovies(userFavorites[0].favoriteMovies); // 
-    navigate(`/frontpage/${user.user}`); // Naviger til frontpage for den valgte brukeren. 
+    setActiveUser(user.user);
+    const userFavorites = await FetchUserFavorites(user.user);
+    setFavoriteMovies(userFavorites[0].favoriteMovies);
+    navigate(`/frontpage/${user.user}`);
   };
 
   return (
@@ -38,26 +35,28 @@ export default function App() {
       <Layout activeUser={activeUser}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route 
-            path="/Bruker-sammenlignet-med/:slug" 
-            element={<UserCompare 
-                        activeUser={activeUser} 
-                        setActiveUser={setActiveUser} 
-                        users={users} 
-                        setUsers={setUsers} 
-                        compareUser={compareUser} 
-                        setCompareUser={setCompareUser} />} 
+          <Route
+            path="/Bruker-sammenlignet-med/:slug"
+            element={
+              <UserCompare
+                activeUser={activeUser}
+                setActiveUser={setActiveUser}
+                users={users}
+                setUsers={setUsers}
+                compareUser={compareUser}
+                setCompareUser={setCompareUser}
+              />
+            }
           />
           <Route path="/Sjanger" element={<Genres content={content} setContent={setContent} />} />
           <Route path="/Sjanger/:slug" element={<OneGenre />} />
-          <Route 
-            path="/frontpage/:user" 
-            element={<FrontPage activeUser={activeUser} favoriteMovies={favoriteMovies} />} 
+          <Route
+            path="/frontpage/:user"
+            element={<FrontPage activeUser={activeUser} favoriteMovies={favoriteMovies} />}
           />
         </Routes>
       </Layout>
 
-      {/* Liste over brukere - skjules hvis en bruker er valgt  */}
       {!activeUser && (
         <div>
           <h1>Brukere</h1>
