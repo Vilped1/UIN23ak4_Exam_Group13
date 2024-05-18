e47fd49355dca4811228ac3c1957ae6eb1491229
Vilped1 <vildeandrea01@gmail.com>
2024-05-18 14:34:10 +0200

    Favoritt sjanger + litt css

--- Frontend/src/components/Genres.jsx
+++ Frontend/src/components/Genres.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 export default function Genres({content, setContent}) {
-  const [active, setActive] = useState()
+  const [active, setActive] = useState([])
+  const [toggleStar, setToggleStar] = useState(false)
+  // const {slug} = useParams()
   // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
   const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
@@ -28,19 +31,48 @@ export default function Genres({content, setContent}) {
     }
   }
 
+  // const handleClick = (clickedStar) => {
+  //   // setToggleStar(clickedStar)
+  //   // setToggleStar(!toggleStar)
+  //   // const favorite += setActive(clickedStar)
+  //   setActive(clickedStar)
+  //   // favorite += {acti}
+  //   // const exist = active.find(item => item.genre === content.genre)
+  //   // setActive((prevActive) =>
+  //   //   exist ? prevActive.map(item => item.genre === content.genre ? { ...item} : item)
+  //   // :
+  //   // [...prevActive, { ...content}])
+  //   // console.log(exist, "finnes")
+  //   console.log(active)
+  //   console.log(toggleStar)
+  // }
+
+  // Chat-GPT
+  const handleClick = (clickedGenre) => {
+    setActive((prevActive) => {
+      if(prevActive.includes(clickedGenre)) {
+        return prevActive.filter((genre) => genre !== clickedGenre)
+      } else {
+        return [...prevActive, clickedGenre]
+      }
+    })
+  }
+  // 
+
   useEffect(() => {
     getGenres()
+    console.log(active)
   }, [active])
 
   return (
     <div id="genreSection">
       {content &&
         content.map((genre, index) => (
-            <li>
+            <li key={index} id={genre}>
             <Link to={"/Sjanger/" + genre.toLowerCase()}><h2>{genre.replaceAll("-", " ")}</h2></Link>
-            <button className="star">
-               <FaRegStar />
-            </button>
+            <span className={`star ${active.includes(genre) ? "active" : ""}`} onClick={() => handleClick(genre)} >
+              {active.includes(genre) ? <FaStar /> : <FaRegStar /> }
+            </span>
             </li>
         ))}
     </div>
