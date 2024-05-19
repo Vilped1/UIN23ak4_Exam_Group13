cc9bf11a58787d10f6e1215e7b851048f88c5e2d
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 13:06:09 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,30 +10,30 @@ export default function App() {
   // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
   /*   const url = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${query}?exact=false&titleType=movie`; */
-  const url =
-    "https://moviesdatabase.p.rapidapi.com/titles?limit=20&startYear=2015&endYear=2023&genre=Action";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  };
+  // const url =
+  //   "https://moviesdatabase.p.rapidapi.com/titles?limit=20&startYear=2015&endYear=2023&genre=Action";
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
+  //     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  //   },
+  // };
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(url, options);
-      const data = await response.json();
-      setContent(data.results);
-      console.log(data.results);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch(url, options);
+  //     const data = await response.json();
+  //     setContent(data.results);
+  //     console.log(data.results);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <>
