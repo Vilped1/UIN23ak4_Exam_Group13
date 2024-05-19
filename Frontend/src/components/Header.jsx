bc2b5ca4cee9ecbfc7b8e6f6d3ef51aae701f907
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 11:19:20 +0200

    Create Header.jsx

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { BiMoviePlay } from "react-icons/bi"
+import { FaUserCircle } from "react-icons/fa"
+
+
+export default function Header() {
+
+    return (
+        <>
+        <header>
+            <Link to="/"><h1>What To See?</h1></Link>
+            <section id="sideheader">
+                <Link to="/"><h3><BiMoviePlay />  Hva skal jeg se?</h3></Link>
+                <Link to="/Sjanger"><h3>Bla gjennom sjangere</h3></Link>
+                <h3><FaUserCircle /> Marius</h3>
+            </section>
+        </header>        
+        </>
+    )
+}
