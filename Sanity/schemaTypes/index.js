3261e36e56f8fe7af2eec7229bba12654a3ab774
thoraho <thoraho@hiof.no>
2024-05-19 08:44:24 +0200

    Update index.js

--- Sanity/schemaTypes/index.js
+++ Sanity/schemaTypes/index.js
@@ -1,6 +1,7 @@
 import { genres } from "./genres"
 import { movies } from "./movies"
 import { images } from "./genres"
+import {users} from "./users"
 
 
-export const schemaTypes = [genres, movies, images]
+export const schemaTypes = [genres, movies, images, users]
