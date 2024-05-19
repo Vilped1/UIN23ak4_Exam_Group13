3801046c8fdedcf396a564d3b863ae18d7b2f90c
thoraho <thoraho@hiof.no>
2024-05-19 10:05:16 +0200

    lagt til objekt i mainuser

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,17 +1,17 @@
-export default function Login() {
-    const handleClick = (e) => {
-        setMainUser(e)
-      }
-    
-      return (
-        <>
-          <h1>Hvem skal se i dag?</h1>
-          <p>Velg bruker</p>
-          {allUsers?.map((user) => (
-            <button key={user._id} onClick={() => handleClick(user)}>
-              {user.user}
-            </button>
-          ))}
-        </>
-      )
+export default function Login({ allUsers, setMainUser }) {
+  const handleClick = (e) => {
+    setMainUser(e)
+  }
+
+  return (
+    <>
+      <h1>Hvem skal se i dag?</h1>
+      <p>Velg bruker</p>
+      {allUsers?.map((user) => (
+        <button key={user._id} onClick={() => handleClick(user)}>
+          {user.user}
+        </button>
+      ))}
+    </>
+  )
 }
