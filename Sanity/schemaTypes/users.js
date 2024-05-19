0f6e34fdba924d2caaeb9f9fd75efa8d37b9829b
AnneOmberg <111729606+AnneOmberg@users.noreply.github.com>
2024-05-19 08:42:50 +0200

    Create users.js

---
+++ Sanity/schemaTypes/users.js
@@ -0,0 +1,34 @@
+export const users = {
+    title: "Brukere",
+    name: "users",
+    type: "document",
+    fields: [
+        {
+            title: "Bruker",
+            name: "user",
+            type: "string"
+        },
+        {
+            title: "Favorittsjangere",
+            name: "favoriteGenre",
+            type: "array",
+            of: [
+                {
+                    type: "reference",
+                    to: [{ type: "genres" }]
+                }
+            ]
+        },
+        {
+            title: "Favorittfilmer",
+            name: "favoriteMovies",
+            type: "array",
+            of: [
+                {
+                    type: "reference",
+                    to: [{ type: "movies" }]
+                }
+            ]
+        }
+    ]
+}
