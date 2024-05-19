458e9e12e983649a20e39704ff6ab9f47709e5f3
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 13:05:38 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,14 +9,14 @@ export default function App() {
   const [content, setContent] = useState(null)
   // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
-  const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  }
+  // const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
+  //     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  //   },
+  // }
 
   return (
     <>
