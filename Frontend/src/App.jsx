8322eca4c20d6824db9b09f8d508793994ab3905
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 10:00:22 +0200

    Flyttet log inn elementene

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -20,19 +20,11 @@ export default function App() {
     getAllUsers()
   }, [])
 
-  const handleClick = (e) => {
-    setMainUser(e)
-  }
+
 
   return (
     <>
-      <h1>Hvem skal se i dag?</h1>
-      <p>Velg bruker</p>
-      {allUsers?.map((user) => (
-        <button key={user._id} onClick={() => handleClick(user)}>
-          {user.user}
-        </button>
-      ))}
+     
     </>
   )
 }
