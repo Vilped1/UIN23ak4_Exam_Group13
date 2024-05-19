a67f77b4d07d5c86ccfd88170d010da5d0d603df
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 09:53:06 +0200

    Små ting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,11 +4,11 @@ import Nav from "./components/Nav"
 import "./App.css"
 import GenreSection from "./components/GenreSection"
 import FetchAllUsers from "../sanity/services/userService"
-import Login from "./components/Login"
+// import Login from "./components/Login"
 
 export default function App() {
   const [allUsers, setAllUsers] = useState([])
-  const [mainUser, setMainUser] = useState(null)
+  const [mainUser, setMainUser] = useState("f0fc50da-74b9-40a8-91bd-7fa8ed61383a")
   console.log(mainUser)
 
   //Henter alle brukere
@@ -24,7 +24,11 @@ export default function App() {
 
   return (
     <>
-      <Login allUsers={allUsers} setMainUser={setMainUser} />
+      <h1>Hei user...!</h1>
+      <h2>Filmer jeg skal se</h2>
+      <h3>Disse filmene ligger i ønskelisten min:</h3>
+      <h2>Jeg skal se sammen med...</h2>
+      {/* <Login allUsers={allUsers} setMainUser={setMainUser} /> */}
     </>
   )
 }
