2ecf0df24e4bdec52ce6453074c7905121c4e502
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 13:06:30 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,25 +10,25 @@ export default function App({ movieCard, setMovieCard }) {
   const [content, setContent] = useState(null)
   // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
-  const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
-      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
-    },
-  }
+  // const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
+  // const options = {
+  //   method: "GET",
+  //   headers: {
+  //     "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
+  //     "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+  //   },
+  // }
 
-  const getGenres = async () => {
-    try {
-      const response = await fetch(url, options)
-      const data = await response.json()
-      setContent(data.results)
-      console.log(data.results)
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  // const getGenres = async () => {
+  //   try {
+  //     const response = await fetch(url, options)
+  //     const data = await response.json()
+  //     setContent(data.results)
+  //     console.log(data.results)
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
 
   useEffect(() => {
     getGenres()
