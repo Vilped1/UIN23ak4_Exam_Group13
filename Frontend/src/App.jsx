97a0b5130db55c48aa90874d7063da024b67a8d7
Vilped1 <vildeandrea01@gmail.com>
2024-05-19 09:57:08 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Nav from "./components/Nav"
 import "./App.css"
 import GenreSection from "./components/GenreSection"
 import FetchAllUsers from "../sanity/services/userService"
-// import Login from "./components/Login"
-// import Login from "./components/Login"
 
 export default function App() {
   const [allUsers, setAllUsers] = useState([])
