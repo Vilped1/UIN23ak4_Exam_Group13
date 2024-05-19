d6b05fdd21aa09a24fbf13f043cf667d1d8c83dd
AnneOmberg <111729606+AnneOmberg@users.noreply.github.com>
2024-05-19 09:48:28 +0200

    Revert "Update App.jsx"
    This reverts commit 9c1b4dfe53a4f103496ae4df262f4450bc924150.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,12 @@ import Nav from "./components/Nav"
 import "./App.css"
 import GenreSection from "./components/GenreSection"
 import FetchAllUsers from "../sanity/services/userService"
-import Login from "./components/Login"
 
 export default function App() {
   const [allUsers, setAllUsers] = useState([])
   const [mainUser, setMainUser] = useState(null)
   console.log(mainUser)
 
-  //Henter alle brukere
   const getAllUsers = async () => {
     const data = await FetchAllUsers()
     setAllUsers(data)
@@ -20,11 +18,20 @@ export default function App() {
   useEffect(() => {
     getAllUsers()
   }, [])
-  //
+
+  const handleClick = (e) => {
+    setMainUser(e)
+  }
 
   return (
     <>
-      <Login allUsers={allUsers} setMainUser={setMainUser} />
+      <h1>Hvem skal se i dag?</h1>
+      <p>Velg bruker</p>
+      {allUsers?.map((user) => (
+        <button key={user._id} onClick={() => handleClick(user._id)}>
+          {user.user}
+        </button>
+      ))}
     </>
   )
 }
