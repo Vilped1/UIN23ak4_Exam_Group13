3b6b2968631b550f39262a369ecc129bb67fef98
thoraho <thoraho@hiof.no>
2024-05-20 11:08:05 +0200

    Filterer api-fetch mot favorittfilmer på brukere

--- frontend/src/components/layout.jsx
+++ frontend/src/components/layout.jsx
@@ -1,30 +1,24 @@
-import Header from "./Header";
+import Header from "./Header"
 
 export default function Layout({ children, logedIn, setLogedIn, mainUser }) {
+  const handleClick = () => {
+    localStorage.setItem("logedIn", false)
+    setLogedIn(false)
+  }
 
-    const handleClick = () => {
-        localStorage.setItem("logedIn", false)
-        setLogedIn(false)
-    }
-
-    return(
+  return (
     <>
-        <div>
-            {
-                logedIn ?
-                <>
-                    <Header mainUser={mainUser}/>
-                    <span>Brukernavn</span>
-                    <button onClick={handleClick}>Logg ut</button>
-                </>
-                :
-                null
-            }
-        </div>
-        <main>
-            {children}
-        </main>
-        <footer>Copyright 2024 UIN</footer>
+      <div>
+        {logedIn ? (
+          <>
+            <Header mainUser={mainUser} />
+            <span>Brukernavn</span>
+            <button onClick={handleClick}>Logg ut</button>
+          </>
+        ) : null}
+      </div>
+      <main>{children}</main>
+      <footer>Copyright 2024 UIN</footer>
     </>
-    )
+  )
 }
