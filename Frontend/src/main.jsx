ce53edd9ff716a7ebf92f0483053f115071398ea
Vilped1 <vildeandrea01@gmail.com>
2024-05-20 12:43:58 +0200

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import "./styles/moviecard.scss";
 import "./styles/Nav.scss";
 import "./styles/GenreSection.scss";
-import "./styles/main.scss";
+import "./styles/Main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
