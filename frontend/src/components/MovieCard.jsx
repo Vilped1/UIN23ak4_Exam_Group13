b3e25092e4f4271e2c4075124ba74236d2fff15c
Erik0631 <111729359+ErikAJensen@users.noreply.github.com>
2024-05-20 17:47:47 +0200

    css

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -8,9 +8,12 @@ import { Link } from "react-router-dom"
 export default function MovieCard({ movie }) {
   return (
     <>
+    <article>
+
       <img src={movie.primaryImage.url} alt={movie.titleText.text} />
       <a href={`https://www.imdb.com/title/tt1114677/${movie.id}`}>{movie.titleText.text}</a>
       <p>{movie.releaseYear.year}</p>
+    </article>
     </>
   )
 }
