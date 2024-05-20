a88b63dbf77f011d2d021cad6e2c7fc5c3251657
Vilped1 <vildeandrea01@gmail.com>
2024-05-20 13:03:37 +0200

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,8 +5,9 @@ import "./index.css"
 import "./styles/moviecard.scss"
 import "./styles/Nav.scss"
 import "./styles/Genre.scss"
-import "./styles/main.scss"
-import "./styles/header.scss"
+import "./styles/Main.scss"
+import "./styles/Header.scss"
+import "./styles/Footer.scss"
 import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
