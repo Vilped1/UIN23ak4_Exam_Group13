0f193cb7170d43b59f4f3e8baa6636ac50e14bb7
Erik0631 <111729359+ErikAJensen@users.noreply.github.com>
2024-05-20 12:00:24 +0200

    fjerner //

--- Frontend/src/components/Genres.jsx
+++ Frontend/src/components/Genres.jsx
@@ -5,20 +5,20 @@ export default function Genres({content, setContent}) {
   const [active, setActive] = useState()
   // API KEY: 9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14
 
-  const url = //"https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
+  const url = "https://moviesdatabase.p.rapidapi.com/titles/utils/genres/"
   const options = {
     method: "GET",
     headers: {
-      //"X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14", 
-      //"X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
+      "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14", 
+      "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
     },
   }
 
- // const getGenres = async () => {
+  const getGenres = async () => {
     try {
       const response = await fetch(url, options)
       const result = await response.json()
-      // Fjerner objektet på indeks 0 i arrayen siden denne er satt til "null"
+      //Fjerner objektet på indeks 0 i arrayen siden denne er satt til "null"
       result.results.splice(0, 1)
       console.log(result.results)
       setContent(result.results)
@@ -26,7 +26,7 @@ export default function Genres({content, setContent}) {
       console.error(error)
     }
   }
-  //useEffect(() => { getGenres()  }, [active])
+  useEffect(() => { getGenres()  }, [active])
 
   return (
     <div id="genreSection">
