48eaa1d47da3ab4f8fef39c59af2a82916cf3b9c
Vilped1 <vildeandrea01@gmail.com>
2024-05-20 17:10:37 +0200

    Opprydning

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -9,7 +9,8 @@ export default function MovieCard({ movie }) {
   return (
     <>
       <img src={movie.primaryImage.url} alt={movie.titleText.text} />
-      <a href={`https://www.imdb.com/title/tt1114677/${movie.id}`}>{movie.titleText.text}</a>
+      {/* Åpne i en ny side */}
+      <a href={`https://www.imdb.com/title/${movie.id}`}>{movie.titleText.text}</a>
       <p>{movie.releaseYear.year}</p>
     </>
   )
