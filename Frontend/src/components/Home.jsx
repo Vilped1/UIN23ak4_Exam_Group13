8c9f25c4e7c6fea86388ac2a12277b0bdc957897
Erik0631 <111729359+ErikAJensen@users.noreply.github.com>
2024-05-20 13:04:33 +0200

    lister opp favroitt filmer

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,18 +1,23 @@
 export default function Home({ mainUser }) {
-    return (
-        <>
+  return (
+      <>
+          {/* Henter brukernavnet*/}
           <h1>Hei {localStorage.getItem("user")}!</h1>
           <article>
-          <section>
-            <h2>Filmer jeg skal se</h2>
-            <p>Disse filmene ligger i ønskelisten min:</p>
-          </section>
-
-          <section>
-            <h2>Jeg skal se sammen med...</h2>
-          </section>
+              <section>
+                  <h2>Filmer jeg skal se</h2>
+                  <p>Disse filmene ligger i ønskelisten min:</p>
+                  <ul>
+                      {/* Lister opp favorittfilmene til mainUser */}
+                      {mainUser.favoriteMovies.map((movie, index) => (
+                          <li key={index}>{movie}</li>
+                      ))}
+                  </ul>
+              </section>
+              <section>
+                  <h2>Jeg skal se sammen med...</h2>
+              </section>
           </article>
-    
-        </>
-      )
+      </>
+  );
 }
