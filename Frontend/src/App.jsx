4233f7244887d877031e64e80b18a71b960417c7
thoraho <thoraho@hiof.no>
2024-05-20 09:35:40 +0200

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MovieCard from "./components/MovieCard";
-import Nav from "./components/Nav";
-import "./App.css";
-import GenreSection from "./components/GenreSection";
-import FetchAllUsers from "../sanity/services/userService";
-import UserCompare from "./components/UserCompare";
-import { fetchAllGenres } from "../sanity/services/genreServices";
-import fetchMovies from "../sanity/services/movieServices";
-import Login from "./components/Login";
-import Home from "./components/Home";
+import { useState, useEffect } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import MovieCard from "./components/MovieCard"
+import Nav from "./components/Nav"
+import "./App.css"
+import GenreSection from "./components/GenreSection"
+import FetchAllUsers from "../sanity/services/userService"
+import UserCompare from "./components/UserCompare"
+import { fetchAllGenres } from "../sanity/services/genreServices"
+import fetchMovies from "../sanity/services/movieServices"
+import Login from "./components/Login"
+import Home from "./components/Home"
 
 export default function App() {
-  const [allGenres, setAllGenres] = useState([]);
-  const [genre, setGenre] = useState([]);
-  const [movies, setMovies] = useState([]);
-  const [allUsers, setAllUsers] = useState([]);
-  const [matchingMovies, setMatchingMovies] = useState([]);
+  const [allGenres, setAllGenres] = useState([])
+  const [genre, setGenre] = useState([])
+
+  const [movies, setMovies] = useState([])
+  const [apiMovies, setApiMovies] = useState([])
+  console.log("API MOVIES", apiMovies)
+
+  const [allUsers, setAllUsers] = useState([])
   const [mainUser, setMainUser] = useState({
     //USER 1
     _id: "badbfdda-8fef-4646-bc8b-3989b8e9e5c9",
@@ -32,7 +35,7 @@ export default function App() {
       },
     ],
     favoriteGenres: null,
-  });
+  })
   //USER 2
   const [compareUser, setCompareUser] = useState({
     _id: "f0fc50da-74b9-40a8-91bd-7fa8ed61383a",
@@ -48,81 +51,71 @@ export default function App() {
       },
     ],
     favoriteGenres: null,
-  });
+  })
 
-  useEffect(() => {
-    const matchingMovies = mainUser.favoriteMovies.filter((mainUserMovies) =>
-      compareUser.favoriteMovies.some(
-        (compareUserMovies) =>
-          compareUserMovies.imdbid === mainUserMovies.imdbid
-      )
-    );
-    setMatchingMovies(matchingMovies);
-  }, []);
-
-  const url = `https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=${matchingMovies
-    .map((movie) => movie.imdbid)
-    .join(",")}`;
+  const url = `https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=${movies.map((movie) => movie.imdbid).join(",")}`
   const options = {
     method: "GET",
     headers: {
       "X-RapidAPI-Key": "9bc8085aa8msh993744cc96d23a2p16fabajsn08b818614d14",
       "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com",
     },
-  };
+  }
 
   const fetchApiMovie = async () => {
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log("API FETCH RESULTS", result.results);
+      const response = await fetch(url, options)
+      const result = await response.json()
+      setApiMovies(result.results)
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   const getAllUsers = async () => {
-    const data = await FetchAllUsers();
-    setAllUsers(data);
-  };
+    const data = await FetchAllUsers()
+    setAllUsers(data)
+  }
 
   useEffect(() => {
-    getAllUsers();
-    getAllGenres();
-    getAllMovies();
-  }, []);
+    getAllUsers()
+    getAllGenres()
+    getAllMovies()
+  }, [])
 
   useEffect(() => {
-    fetchApiMovie();
-  }, [movies]);
+    fetchApiMovie()
+  }, [movies])
 
   const getAllGenres = async () => {
-    const data = await fetchAllGenres();
-    setAllGenres(data);
-  };
+    const data = await fetchAllGenres()
+    setAllGenres(data)
+  }
 
   const getAllMovies = async () => {
-    const data = await fetchMovies();
-    setMovies(data);
+    const data = await fetchMovies()
+    setMovies(data)
     /* console.log("MOVIES DATA", data); */
-  };
+  }
 
   return (
-    /* {movies?.map((movie, index) => (
-        <div key={index}>
-          <h2>{movie.movietitle}</h2>
-           <p>{movie.imdbid}</p>
-           </div> */
+    <>
+      {apiMovies
+        .filter((movie) => movie.imdbid === mainUser.favoriteMovies.imdbid)
+        .map((movie) => (
+          <div>
+            <img src={movie.primaryImage.url} alt={movie.title} />
+            <h1>{movie.titleText.text}</h1>
+          </div>
+        ))}
+    </>
 
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<Login allUsers={allUsers} setMainUser={setMainUser} />}
-        />
-        <Route path="/home" element={<Home mainUser={mainUser} />} />
-        {/* Route til valgt bruker*/}
-      </Routes>
-    </Router>
-  );
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Login allUsers={allUsers} setMainUser={setMainUser} />} />
+    //     <Route path="/home" element={<Home mainUser={mainUser} />} />
+    //     {/* Route til valgt bruker*/}
+    //   </Routes>
+    // </Router>
+  )
 }
