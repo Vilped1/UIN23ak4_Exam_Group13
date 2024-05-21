71490e3bc4b404255ae009d501f43edb2f2563b5
JesperMyhre <111729406+JesperMyhre@users.noreply.github.com>
2024-05-21 07:45:42 +0200

    lagt til metoder

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
-import { IconContext } from "react-icons"
-import { FaRegHeart } from "react-icons/fa"
-import { FaHeart } from "react-icons/fa"
-import heartIcon from "../assets/heartIcon.svg"
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { IconContext } from "react-icons";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import heartIcon from "../assets/heartIcon.svg";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function MovieCard({ movie }) {
   return (
     <>
-      <a href={`https://www.imdb.com/title/${movie.id}/`}>
+      <a href={`https://www.imdb.com/title/${movie.id}/`} target="_blank">
         <img src={movie.primaryImage.url} alt={movie.titleText.text} />
       </a>
       <section id="movieInfo">
@@ -16,5 +16,5 @@ export default function MovieCard({ movie }) {
         <p>{movie.releaseYear.year}</p>
       </section>
     </>
-  )
+  );
 }
